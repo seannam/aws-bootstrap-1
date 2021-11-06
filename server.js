e6519ed16b248745066dc418532b1e72c4a595e3
Sean Nam <namsangwoo1@gmail.com>
2021-11-06 16:16:14 -0700

    install express

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const { hostname } = require('os');
 const https = require('https');
 const fs = require('fs');
+const express = require('express')
 
 const STACK_NAME = process.env.STACK_NAME || "Unknown Stack";
 const httpsPort = 8443;
 const httpsKey = '../keys/key.pem'
 const httpsCert = '../keys/cert.pem'
+
+const app = express()
 
 if (fs.existsSync(httpsKey) && fs.existsSync(httpsCert)) {
     console.log('Starting https server')
@@ -16,9 +19,16 @@ if (fs.existsSync(httpsKey) && fs.existsSync(httpsCert)) {
         res.setHeader('Content-Type', 'text/plain');
         res.end(message);
     });
-    server.listen(httpsPort, hostname, () => {
+    app.listen(httpsPort, hostname, () => {
         console.log(`Server running at http://${hostname()}:${httpsPort}/`);
     });
+
+    app.get("/", (req, res) => {
+        res.statusCode = 200;
+        res.send("<h1>Hello World!</h1>");
+    });
+
+
 } else {
     console.log('Could not find certificate/key')
 }
